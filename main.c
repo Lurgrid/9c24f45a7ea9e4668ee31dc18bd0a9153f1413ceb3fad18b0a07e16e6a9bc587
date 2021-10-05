a99104e898b92ec15d0ed0cc840c4a10d2e910d7
Lurgrid <Lurgrid@gmail.com>
2021-10-05 09:14:08 +0200

    0.0

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ int main(void){
     printf("Donne moi ton age. \n");
     scanf("%d", &age);
     if(age >= 18){
-        printf("Tu es majeur car tu as %d. \n", &age);
+        printf("Tu es majeur car tu as %d. \n", age);
     }else{
-        printf("Tu es mineur car tu as %d. \n", &age);
+        printf("Tu es mineur car tu as %d. \n", age);
     };
     return EXIT_SUCCESS;
 };

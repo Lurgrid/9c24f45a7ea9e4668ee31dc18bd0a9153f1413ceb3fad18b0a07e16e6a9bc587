0ad3a4173441a1163e8a673951f394079059d2c4
Lurgrid <Lurgrid@gmail.com>
2021-10-18 08:37:59 +0200

    18/10/2021

---
+++ b3abe5d8c69b38733ad57ea75e83bcae42bbbbac75e3a5445862ed2f8a2cd677/b3abe5d8c69b38733ad57ea75e83bcae42bbbbac75e3a5445862ed2f8a2cd677.js
@@ -0,0 +1,12 @@
+var scanf = require('scanf');
+var sha256 = require('js-sha256');
+
+console.log("Entrez le mots a hash")
+
+let mot = scanf('%s');
+
+var hash = sha256.create();
+hash.update(mot);
+//hash.array();
+
+console.log(hash.hex())

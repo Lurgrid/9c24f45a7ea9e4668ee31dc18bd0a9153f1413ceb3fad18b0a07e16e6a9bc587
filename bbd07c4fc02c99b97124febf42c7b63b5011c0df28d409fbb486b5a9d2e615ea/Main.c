f7a4da8de9e6e558f52095f6fb9798b5c6961353
Lurgrid <Lurgrid@gmail.com>
2021-10-27 09:58:08 +0200

    27/10/2021

--- bbd07c4fc02c99b97124febf42c7b63b5011c0df28d409fbb486b5a9d2e615ea/Main.c
+++ bbd07c4fc02c99b97124febf42c7b63b5011c0df28d409fbb486b5a9d2e615ea/Main.c
@@ -10,12 +10,17 @@ int main(void) {
 		printf("No memory\n");
 		return 1;
 	}
-	printf("\n Entrez une commande (ping etc...)\n ");
-	fgets(name, 100, stdin);
+	while (1 == 1) {
+		printf("\n Entrez une commande (ping etc...)\n ");
+		fgets(name, 100, stdin);
 
-	if ((strlen(name) > 0) && (name[strlen(name) - 1] == '\n'))
-		name[strlen(name) - 1] = '\0';
-	system(name);
+		if ((strlen(name) > 0) && (name[strlen(name) - 1] == '\n'))
+			name[strlen(name) - 1] = '\0';
+		system(name);
+		for (int i = 0; name[i]; i++) {
+			name[i] = tolower(name[i]);
+		}
+	}
 	system("pause");
 
 	return EXIT_SUCCESS;
